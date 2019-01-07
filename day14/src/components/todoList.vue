142f35375675ee2d6d2ff4b455a6e0db869f4028
ke102882 <acatact8228@gmail.com>
2019-01-08 00:35:11 +0800

    bug=todoIndex

---
+++ day14/src/components/todoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="todo-list-item">
+    <input type="checkbox">
+    <label>{{ todo.content }}</label>
+    <button class="destroy">{{ index }}</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'destroy',
+  props: {
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    todo() {
+      // return this.$store.state.todos[this.index];
+      return this.$store.state.todos[this.index];
+    }
+  }
+}
+</script>
+      <!-- <div class="todo-list-item" v-for="index in todoIndex" :key="index" :index="index">
+        <input type="checkbox" v-model="">
+        <label for="">{{ todo.content }}</label>
+        <button class="destroy" @click="destroyHandler"></button>
+      </div> -->
--- day14/src/views/todoList/index.vue
+++ day14/src/views/todoList/index.vue
@@ -1,20 +1,43 @@
+<template>
+  <section>
+    <div class="container">
+      <router-link :to="{name:'all'}">全部</router-link> |
+      <router-link :to="{name:'active'}">未完成</router-link> |
+      <router-link :to="{name:'complete'}">已完成</router-link> |
+      <div class="todo-input">
+        <input
+          type="text"
+          placeholder="請輸入"
+          v-model.trim="todo"
+          @keyup.enter="submitHanlder"
+          v-focus
+        />
+      </div>
+      <div class="todo-List">
+        <TODOListItem v-for="index in todoIndex":key="index" :index="index"/>
+      </div>
+    </div>
+  </section>
+</template>
+
 <script>
+import TODOListItem from '@/components/todoList.vue';
+
 export default {
   data() {
     return {
       todo: ""
     };
   },
-  computed: {
-    todoGo() {
-      return this.$store.state.todos[this.index];
-    },
-    todoIndex() {
-      return { id:1,id:2}
-    }
+  props: {
+    todoIndex: String
+  },
+  components:{
+    TODOListItem
   },
   methods: {
-    submitHanlder() {
+      
+      submitHanlder() {
       if (!this.todo) return;
       this.$store.commit("Add_Todos", {
         content: this.todo,
@@ -25,7 +48,3 @@ export default {
   }
 };
 </script>
-
-
-
-<template src="./template.html"></template>
